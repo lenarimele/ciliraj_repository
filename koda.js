2ec77860241440efd97b4c96614b4e68b128f53e
lenarimele <lena.rimele@student.um.si>
2025-04-23 20:02:09 +0200

    Mali del prehodov med stranmi. Mora se še dokončat.

---
+++ koda.js
@@ -0,0 +1,11 @@
+function profil() {
+    window.location.href = "profile.html";
+}
+
+function kosarica() {
+    window.location.href = "košarica.html";
+}
+
+function domov() {
+    window.location.href = "HOMEPAGE.html";
+}
